99a667eb6395b18750b2c6d3e9966f3ffd97cd97
l00143533 <l00143533@l00143533-virtual-machine>
2021-01-04 21:29:49 +0000

    Added test

---
+++ JavaProject/src/ie/lyit/test/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package ie.lyit.test;
+
+public class TestHelloWorld {
+	
+	static int num = 5;
+
+	public static void main(String[] args) {
+		
+			
+		if(num == 5)
+			System.out.print("True");
+
+	}
+
+}
